2e0eb6c8235db5f68065f643e14306a1f49a9528
ksrisayee12 <ksrisayee@gmail.com>
2021-08-02 20:23:02 +0530

    WILY 2

--- App.js
+++ App.js
@@ -1,21 +1,81 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, TouchableOpacity, StyleSheet, Image } from 'react-native';
+import * as Permissions from 'expo-permissions'
+import {BarCodeScanner} from 'expo-barcode-scanner'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+export default class TransactionScreen extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      hasCamPermisson: null,
+      scanned: false,
+      scannedData: '',
+      buttonState: 'normal'
+    }
+  }
+  getCameraPermissions = async()=>{
+    const {status} = await Permissions.askAsync(Permissions.CAMERA)
+    this.setState({
+      hasCamPermisson: status === 'granted',
+      buttonState: 'clicked',
+      scanned: false
+    })
+  }
+  handleBarCodeScanned = async({type, data}) => {
+    this.setState({
+      scanned: true,
+      scannedData: data,
+    })
+  }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    render() {
+      const hasCamPermisson = this.state.hasCamPermisson;
+      const scanned = this.state.scanned;
+      const scannedData = this.state.scannedData;
+      const buttonState = this.state.buttonState
+      if (buttonState === 'clicked' && hasCamPermisson){
+        return(
+          <BarCodeScanner
+            onBarCodeScanned = {scanned ? undefined : this.handleBarCodeScanned}
+            style={StyleSheet.absoluteFillObject} />
+        )
+      }
+      else if(buttonState === 'normal'){
+      return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Image source={require('./assets/barcode.png')} style = {style.Img} />
+          <Text style={style.Font}> {
+            hasCamPermisson === true ? this.state.scannedData : "requestCamPermission"
+          } </Text>
+          <TouchableOpacity onPress={this.getCameraPermissions}><Text style={style.ButtonText}>Scan QR Code</Text></TouchableOpacity>
+        </View>
+      );
+      }
+    }
+  }
+
+const style = StyleSheet.create({
+  ButtonText: {
+    padding: 24,
+    marginTop: 16,
+    paddingVertical: 8,
+    borderWidth: 4,
+    borderColor: "#20232a",
+    borderRadius: 6,
+    backgroundColor: "#61dafb",
+    color: "#20232a",
+    textAlign: "center",
+    fontSize: 30,
+    fontWeight: "bold"
   },
-});
+  Font: {
+    fontSize: 20,
+    marginBottom: 16,
+  },
+  Img: {
+    width: 200,
+    height: 200,
+    marginTop: -130,
+    marginBottom: 55
+  },
+})
